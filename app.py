286b9f9dd057ae3386a6a5be150552f7988ba060
Maddi Rishi Dhaneswar <149222212+RishiMaddi1@users.noreply.github.com>
2025-05-04 16:23:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def subtract_credits():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080, debug=True)
